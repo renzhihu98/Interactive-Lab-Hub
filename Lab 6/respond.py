2e8fc89f8341704addc9717fe836919d486225f4
Renzhi Hu <renzhihu98@gmail.com>
2021-05-04 16:46:56 -0400

    edit files

--- Lab 6/respond.py
+++ Lab 6/respond.py
@@ -1,12 +1,13 @@
-import board
-import digitalio
-from adafruit_apds9960.apds9960 import APDS9960
-import time
 import paho.mqtt.client as mqtt
 import uuid
+import qwiic_button
+import time
 import subprocess
+import digitalio
+import board
 import adafruit_rgb_display.st7789 as st7789
 from PIL import Image, ImageDraw, ImageFont
+
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -32,6 +33,9 @@ disp = st7789.ST7789(
     y_offset=40,
 )
 
+
+
+
 # Create blank image for drawing.
 # Make sure to create image with mode 'RGB' for full color.
 height = disp.width  # we swap height/width to rotate it to landscape!
@@ -54,24 +58,21 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
+
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 28)
+
 
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
-i2c = board.I2C()
-apds = APDS9960(i2c)
+# Topic for sending messages
+send_topic = "IDD/respond"
+# Topic for reading messages
+receive_topic = "IDD/proximity"
 
-apds.enable_proximity = True
-
-send_topic = 'IDD/proximity'
-receive_topic = 'IDD/respond'
-
-available = False
-
-yes_text = "Yuanhao wants to chat with you"
-no_text = "Yuanhao is busy"
+on_text = "I'm available now, feel free to reach out."
+away_text = "I'm away."
 
 
 #this is the callback that gets called once we connect to the broker.
@@ -83,39 +84,42 @@ def on_connect(client, userdata, flags, rc):
 # this is the callback that gets called each time a message is recived
 def on_message(client, userdata, msg):
     global status
+    
 
-    print(f"{msg.payload.decode('UTF-8')}")
+    print(f"topic: {msg.topic} msg: {msg.payload.decode('UTF-8')}")
     text = msg.payload.decode('UTF-8')
 
-    if not text:
+    if text == on_text:
+        buttonGreen.LED_on(100)
+        buttonRed.LED_on(100)
         y = top
-        screen_text = "Waiting for"
-        screen_text2 = "Yuanhao's response"
+        screen_text = "Renzhi is"
+        screen_text2= "available now!"
         draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-        draw.text((x, y), screen_text, font=font, fill="#FFFFFF")
-        draw.text((x, y+30), screen_text2, font=font, fill="#FFFFFF")
+        draw.text((x, y), screen_text, font=font, fill="#88CA35")
+        draw.text((x, y+30), screen_text2, font=font, fill="#88CA35")
         disp.image(image, rotation)
-    
-    else:
-        if text == yes_text:
-            y = top
-            screen_text = "Yuanhao wants"
-            screen_text2= "to chat with you!"
-            draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-            draw.text((x, y), screen_text, font=font, fill="#88CA35")
-            draw.text((x, y+30), screen_text2, font=font, fill="#88CA35")
-            disp.image(image, rotation)
-            time.sleep(2)
-        else:
-            y = top
-            screen_text = "Yuanhao is"
-            screen_text2= "busy."
-            draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-            draw.text((x, y), screen_text, font=font, fill="#FF6666")
-            draw.text((x, y+30), screen_text2, font=font, fill="#FF6666")
-            disp.image(image, rotation)
-            time.sleep(2)
+        time.sleep(2)
 
+    else:
+        buttonGreen.LED_off()
+        buttonRed.LED_off()
+        y = top
+        screen_text = "Renzhi is"
+        screen_text2= "not here."
+        draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+        draw.text((x, y), screen_text, font=font, fill="#FF6666")
+        draw.text((x, y+30), screen_text2, font=font, fill="#FF6666")
+        disp.image(image, rotation)
+        time.sleep(2)
+
+    y = top
+    screen_text = "Looking for"
+    screen_text2 = "Renzhi..."
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    draw.text((x, y), screen_text, font=font, fill="#FFFFFF")
+    draw.text((x, y+30), screen_text2, font=font, fill="#FFFFFF")
+    disp.image(image, rotation)
 
 # Every client needs a random ID
 client = mqtt.Client(str(uuid.uuid1()))
@@ -131,15 +135,26 @@ client.on_message = on_message
 #connect to the broker
 client.connect('farlab.infosci.cornell.edu',port=8883)
 
-client.loop_start()
+# Initialize all LED buttons
+buttonGreen = qwiic_button.QwiicButton(0x33)
+buttonRed = qwiic_button.QwiicButton(0x66)
 
+buttonGreen.begin()
+buttonRed.begin()
+
+buttonGreen.LED_off()
+buttonRed.LED_off()
+
+
+# Loop to read in the input from controller to send to dancer
 while True:
-    # print(f'{apds.proximity}')
-    if apds.proximity > 5 and not available:
-        client.publish(send_topic, "I'm available now, feel free to reach out.")
-        available = True
-    elif 0 <= apds.proximity <= 5 and available:
-        client.publish(send_topic, "I'm away.")
-        available = False
+    client.loop()
 
-    time.sleep(.5)
+    if buttonGreen.is_button_pressed():
+        client.publish(send_topic, "Yuanhao wants to chat with you")
+        status = 'YES'
+    elif buttonRed.is_button_pressed():
+        client.publish(send_topic, "Yuanhao is busy")
+        status = 'NO'
+
+    time.sleep(0.5)
